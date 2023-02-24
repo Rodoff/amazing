cae7c9baae988cae0487a60fb80253f2c8d7266a
RoDoff <trobianirodolfo@gmail.com>
2023-02-24 00:23:26 -0300

    script

---
+++ script/script.js
@@ -0,0 +1,32 @@
+console.log(data);
+const eventos = data.events;
+console.log(eventos)
+let otroeven = []
+
+function printEvents() {
+    for (let event of eventos) {
+        let listaev = `
+        <div class="contene  d-flex">
+    <div class="card" style="width: 18rem;">
+    <img src="${event.image}" class="card-img-top" alt="${event.name}">
+    <div class="card-body">
+        <h5 class="card-title">${event.name}</h5>
+        <p class="card-text">${event.description}</p>
+        <a href="./details.html" class="btn btn-primary">Details</a>
+        </div>
+        </div> 
+    `
+        otroeven.push(listaev)
+
+    }
+}
+function imprimir() {
+    let listaeventos = document.getElementById('cardEvents')
+    listaeventos.innerHTML = otroeven.join('')
+}
+console.log(otroeven)
+printEvents();
+imprimir();
+
+
+
