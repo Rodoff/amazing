d3ff51b2223624dc9c556770b584f81e5b81617c
RoDoff <trobianirodolfo@gmail.com>
2023-03-17 16:33:41 -0300

    arreglos3

--- script/stats1.js
+++ script/stats1.js
@@ -15,7 +15,7 @@ async function printabla1() {
         document.getElementById("max").innerHTML = eventos[eventos.length - 1].name;
         document.getElementById("maxasis").innerHTML = eventos[eventos.length - 1].porcentaje + "%";
         document.getElementById("min").innerHTML = eventos[0].name;
-        document.getElementById("minasis").innerHTML = eventos[0].porcentaje + "%";
+        document.getElementById("minasis").innerHTML = eventos[0].porcentaje  + "%";
 
 
         eventos = eventos.sort((a, b) => a.capacity - b.capacity)
